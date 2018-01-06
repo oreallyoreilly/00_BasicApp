1138785d16db0b4b5847fa6b5dcaf8adcc699134
Conor <conor@CPOR-MAC-115.local>
2018-01-06 14:17:36 +0000

    initial commit

---
+++ src/main/java/com/oreallyoreilly/BasicApp/App.java
@@ -0,0 +1,60 @@
+package com.oreallyoreilly.BasicApp;
+
+import java.util.Date;
+import java.util.Scanner;
+
+/*****************************************************************
+ *
+ *	Date: 2017
+ *	@author COR
+ *  
+ *
+ *****************************************************************/
+
+public class App
+{ 
+	
+	public static void main(String args[])
+	{
+			App anApp = new App();
+	}
+	
+	// DATA
+	//............................................................
+	//declare objects
+	private	Scanner someInput;
+	private Date today;
+	
+	// CONSTRUCTORS
+	//............................................................
+	public App()
+	{
+		//initialise variables
+		
+        //create objects 
+		this.someInput = new Scanner(System.in);
+		
+		//Starting method
+        sayHi();		
+		
+		//pause before exit (this is only useful if an error occurs)
+        System.out.println(" \n Press enter to exit the program");
+		this.someInput.nextLine();
+
+		//close the program without error
+		System.exit(0);
+	}
+	
+	// METHODS
+	//............................................................
+	
+	private void sayHi()
+	{
+
+		this.today = new Date();
+		System.out.println( "App says Hi at " + today );
+		
+	}//EOM
+	
+}//EOC
+
